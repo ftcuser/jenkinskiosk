82a278ae1476c7c63c962ee723644d2d01385339
ftcuser <swang@citizant.com>
2017-02-17 14:51:24 -0500

    fix dashbord cache issue

--- jenkinskiosk/src/main/java/com/citizant/jenkinsmanager/service/JenkinsReportServiceImpl.java
+++ jenkinskiosk/src/main/java/com/citizant/jenkinsmanager/service/JenkinsReportServiceImpl.java
@@ -34,158 +34,137 @@ import com.offbytwo.jenkins.model.Job;
 import com.offbytwo.jenkins.model.JobWithDetails;
 
 public class JenkinsReportServiceImpl implements JenkinsReportService {
-	
+
 	@Autowired
 	private JenkinsNodesDao jenkinsNodesDao;
 
 	private SimpleDateFormat SF = new SimpleDateFormat("MM/dd/yyyy");
-	protected Dashboard dashboard = null;
-	
+
 	public List<JenkinsNode> getJenkinsNodes(String configFile) {
-		/*
-		ObjectMapper mapper = new ObjectMapper();
-		String json;
-		
-		try {
-			json = new String(Files.readAllBytes(Paths.get(configFile)));
-			nodes = mapper.readValue(json, new TypeReference<List<JenkinsNode>>(){});
-			
-			//Check if the servers are running
-			for(JenkinsNode node : nodes) {
-				JenkinsServer js = new JenkinsServer(new URI(node.getServerUrl()), node.getUsername(), node.getPassword());
-				node.setRunning(js.isRunning());
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}  
-		
-		return nodes;
-		*/
 		List<JenkinsNode> nodes = new ArrayList<JenkinsNode>();
 		try {
 			List<com.citizant.jenkinsmanager.domain.JenkinsNode> jenkinsNodesList = jenkinsNodesDao.getNodes();
-			
-			//Check if the servers are running
+
+			// Check if the servers are running
 			nodes.clear();
-			for(com.citizant.jenkinsmanager.domain.JenkinsNode jenkinsNode : jenkinsNodesList) {
+			for (com.citizant.jenkinsmanager.domain.JenkinsNode jenkinsNode : jenkinsNodesList) {
 				JenkinsNode node = mapJenkinsNodeToUINode(jenkinsNode);
 				nodes.add(node);
-				JenkinsServer js = new JenkinsServer(new URI(node.getServerUrl()), node.getUsername(), node.getPassword());
+				JenkinsServer js = new JenkinsServer(new URI(node.getServerUrl()), node.getUsername(),
+						node.getPassword());
 				node.setRunning(js.isRunning());
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}  
-		
+		}
+
 		return nodes;
 	}
-	
+
 	public Dashboard getCloudDashboard(List<JenkinsNode> nodes) {
-		
+
 		Dashboard ds = null;
-		//Retrieve from database first
+		// Retrieve from database first
 		String json = jenkinsNodesDao.getValue("dashboard");
-		
-		if(json != null) {
+
+		if (json != null) {
 			ObjectMapper mapper = new ObjectMapper();
 			try {
-			
-				ds = mapper.readValue(json, new TypeReference<Dashboard>(){});
-				
+
+				ds = mapper.readValue(json, new TypeReference<Dashboard>() {
+				});
+
 			} catch (Exception e) {
 				e.printStackTrace();
-			}  
+			}
 		} else {
 			ds = getAllJenkinsStatistics(nodes);
 		}
 		return ds;
-		
+
 	}
 
 	public Dashboard getAllJenkinsStatistics(List<JenkinsNode> nodes) {
-		if(dashboard == null) {
-			dashboard = new Dashboard();
-			HashMap<String, BuildStatistics> bumap = new HashMap<String, BuildStatistics>();
-			
-			for(JenkinsNode node : nodes) {
-					dashboard.setNumOfNodes(dashboard.getNumOfNodes() + 1);
-					if(node.isRunning()) {
-						dashboard.setNumOfLiveNodes(dashboard.getNumOfLiveNodes() + 1);
-						countJenkins(dashboard, bumap, node);
-					}				
-			}			
-			
-			//Process results to list
-				
-			List<BuildStatistics> buildscs = new ArrayList<BuildStatistics>();
-			buildscs.addAll(bumap.values());
-			
-			//Sort by time stamp
-			Collections.sort(buildscs, new Comparator<BuildStatistics>() {
-				   public int compare(BuildStatistics b1, BuildStatistics b2) {
-				      if( b1.getDatestamp() > b2.getDatestamp()) return 1;
-				      else return -1; 
-				   }
-				});
-			dashboard.setBuildscs(buildscs);
+		Dashboard dashboard = new Dashboard();
+		HashMap<String, BuildStatistics> bumap = new HashMap<String, BuildStatistics>();
+
+		for (JenkinsNode node : nodes) {
+			dashboard.setNumOfNodes(dashboard.getNumOfNodes() + 1);
+			if (node.isRunning()) {
+				dashboard.setNumOfLiveNodes(dashboard.getNumOfLiveNodes() + 1);
+				countJenkins(dashboard, bumap, node);
+			}
 		}
-		
+
+		// Process results to list
+
+		List<BuildStatistics> buildscs = new ArrayList<BuildStatistics>();
+		buildscs.addAll(bumap.values());
+
+		// Sort by time stamp
+		Collections.sort(buildscs, new Comparator<BuildStatistics>() {
+			public int compare(BuildStatistics b1, BuildStatistics b2) {
+				if (b1.getDatestamp() > b2.getDatestamp())
+					return 1;
+				else
+					return -1;
+			}
+		});
+		dashboard.setBuildscs(buildscs);
+
 		return dashboard;
 	}
-	
+
 	private void countJenkins(Dashboard ds, HashMap<String, BuildStatistics> bumap, JenkinsNode node) {
-		try{
+		try {
 			JenkinsServer js = new JenkinsServer(new URI(node.getServerUrl()), node.getUsername(), node.getPassword());
 
 			Map<String, Job> jobMap = js.getJobs();
 			Iterator<String> keys = jobMap.keySet().iterator();
-			
-			while(keys.hasNext()) {
+
+			while (keys.hasNext()) {
 				JobWithDetails jd = jobMap.get(keys.next()).details();
 				List<Build> builds = jd.getAllBuilds();
-				for(Build b : builds) {
+				for (Build b : builds) {
 					BuildWithDetails bwd = b.details();
 					long dstamp = getBuildDate(bwd.getTimestamp());
 					String bdate = getBuildDateStr(bwd.getTimestamp());
-					
+
 					BuildStatistics bds = bumap.get(bdate);
-					if(bds == null) {
+					if (bds == null) {
 						bds = new BuildStatistics();
 						bds.setDatestamp(dstamp);
 						bds.setDate(bdate);
 					}
-					
-					if( bwd.getResult().equals(BuildResult.SUCCESS)){
+
+					if (bwd.getResult().equals(BuildResult.SUCCESS)) {
 						bds.setSucessful(bds.getSucessful() + 1);
 						ds.setNumOfSuccessful(ds.getNumOfSuccessful() + 1);
 					} else {
 						bds.setFailed(bds.getFailed() + 1);
 						ds.setNumOfFailed(ds.getNumOfFailed() + 1);
 					}
-					bumap.put(bdate, bds);					
+					bumap.put(bdate, bds);
 				}
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	private long getBuildDate(long timestamp) {
 		String dd = SF.format(new Date(timestamp));
 		try {
 			return SF.parse(dd).getTime();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return timestamp;
 	}
 
-	
 	private String getBuildDateStr(long timestamp) {
 		return SF.format(new Date(timestamp));
 	}
@@ -201,13 +180,13 @@ public class JenkinsReportServiceImpl implements JenkinsReportService {
 		node.setActive(jenkinsNode.isActive());
 		return node;
 	}
-	
-	@Scheduled(fixedRate=60000)
+
+	@Scheduled(fixedRate = 60000)
 	public void generateCloudStatistics() {
 		List<JenkinsNode> nodes = getJenkinsNodes(null);
 		Dashboard dbs = getAllJenkinsStatistics(nodes);
-		
-		//Save results to database
+
+		// Save results to database
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 		try {
@@ -220,5 +199,5 @@ public class JenkinsReportServiceImpl implements JenkinsReportService {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
